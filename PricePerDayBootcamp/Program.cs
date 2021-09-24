b08156f55b1898fa0ac117234d5982badb449fa2
Jason Ryan <jamariryan89@yahoo.com>
2021-09-24 08:57:36 -0500

    Add project files.

---
+++ PricePerDayBootcamp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PricePerDayBootcamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CostOfBootcamp = 15500.0;
+            var NumberOfDays = 65;
+            var PricePerDay = CostOfBootcamp / NumberOfDays;
+            Console.WriteLine($"The boot camp costs {PricePerDay} per day.");
+        }
+    }
+}
